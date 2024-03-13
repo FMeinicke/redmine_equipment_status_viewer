13a67fc9d81ced68e941ab441900411b12d0af38
Florian Meinicke <florian.meinicke@cetoni.de>
2024-03-13 09:13:57 +0100

    Add 006_add_project_id_to_equipment_assets.rb
    to fix an internal server error when performing a search from any
    project. Without this migration the `equipment_assets` table in the DB
    does not have a `project_id` column causing the search to fail
    (`ActiveRecord::StatementInvalid (PG::UndefinedColumn: ERROR:  column
    equipment_assets.project_id does not exist`). The code of this plugin
    already contained a hack to *"placate the use of project_id in search
    view"* (see app/models/equipment_asset.rb) but this was not enough as
    it turns out.

---
+++ db/migrate/006_add_project_id_to_equipment_assets.rb
@@ -0,0 +1,29 @@
+# Redmine Equipment Status Viewer - An equipment manager plugin
+# Copyright (C) 2010-2011  Devin Weaver
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+class AddProjectIdToEquipmentAssets < ActiveRecord::Migration[4.2]
+  def self.up
+    add_column :equipment_assets, :project_id, :integer
+  end
+
+  def self.down
+    remove_column :equipment_assets, :project_id
+  end
+end
+
+
+
